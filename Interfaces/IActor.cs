e61b801c54581d52a4b0ab69f453fa4da9721502
salkam3156 <salkam3156@hotmail.com>
2019-03-30 10:16:45 +0100

    trimming interface down for now

--- Interfaces/IActor.cs
+++ Interfaces/IActor.cs
@@ -1,9 +1,10 @@
+using SFMLCore.Interfaces;
+
 namespace SFMLSharp.Interfaces
 {
-    public interface IActor
+    public interface IActor : IEntity
     {
          void MoveLeft();
          void MoveRight();
-         void Jump();
     }
 }
